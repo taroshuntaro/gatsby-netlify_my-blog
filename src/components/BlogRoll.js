87b35d61a539a0a440a085228881efe30aca88a2
shuntaro shoji <160341ss@gmail.com>
2019-09-07 23:40:20 +0900

    不要なページ、ファイルを削除 + フォーマッター再適用

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,7 +1,7 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Link, graphql, StaticQuery } from "gatsby";
-import PreviewCompatibleImage from "./PreviewCompatibleImage";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, graphql, StaticQuery } from 'gatsby';
+import PreviewCompatibleImage from './PreviewCompatibleImage';
 
 class BlogRoll extends React.Component {
   render() {
@@ -12,10 +12,10 @@ class BlogRoll extends React.Component {
       <div className="columns is-multiline">
         {posts &&
           posts.map(({ node: post }) => (
-            <div className="is-parent column is-4" key={post.id}>
+            <div className="is-parent column is-12" key={post.id}>
               <article
                 className={`blog-list-item tile is-child box notification ${
-                  post.frontmatter.featuredpost ? "is-featured" : ""
+                  post.frontmatter.featuredpost ? 'is-featured' : ''
                 }`}
               >
                 <header>
