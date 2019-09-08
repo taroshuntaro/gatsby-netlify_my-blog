e3e57d2e818f721c7fd023374435fadd387cf723
shuntaro shoji <160341ss@gmail.com>
2019-09-08 23:06:04 +0900

    [Footer] Githubリンク追加

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import github from '../img/github-icon.svg';
 import facebook from '../img/social/facebook.svg';
 import instagram from '../img/social/instagram.svg';
 import twitter from '../img/social/twitter.svg';
@@ -15,14 +16,21 @@ const Footer = class extends React.Component {
               <div className="column is-4"></div>
               {/* ▲▲▲ offset ※is-offset-x が効かない為 ▲▲▲ */}
               <div className="column is-4 social">
-                <a title="facebook" href="https://facebook.com">
+                <a title="facebook" href="https://github.com/taroshuntaro/">
+                  <img
+                    src={github}
+                    alt="Github"
+                    style={{ width: '1em', height: '1em' }}
+                  />
+                </a>
+                <a title="facebook" href="https://ja-jp.facebook.com/">
                   <img
                     src={facebook}
                     alt="Facebook"
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="twitter" href="https://twitter.com">
+                <a title="twitter" href="https://twitter.com/">
                   <img
                     className="fas fa-lg"
                     src={twitter}
@@ -30,7 +38,7 @@ const Footer = class extends React.Component {
                     style={{ width: '1em', height: '1em' }}
                   />
                 </a>
-                <a title="instagram" href="https://instagram.com">
+                <a title="instagram" href="https://www.instagram.com/?hl=ja">
                   <img
                     src={instagram}
                     alt="Instagram"
